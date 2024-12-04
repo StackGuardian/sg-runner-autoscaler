2d170339f57141e7d5ee1273b0054ed99fba783d
Taher Kathanawala <taher.kathanawala@stackguardian.io>
2024-12-04 18:38:53 +0530

    set the script to run every minute

--- function_app.py
+++ function_app.py
@@ -12,7 +12,7 @@ app = func.FunctionApp()
 
 
 @app.timer_trigger(
-    schedule="* * * * * *",
+    schedule="0 * * * * *",
     arg_name="myTimer",
     run_on_startup=False,
     use_monitor=False,
